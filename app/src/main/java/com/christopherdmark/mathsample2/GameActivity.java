5acdef792927524f7740d0433345cc3ac43dbc6e
Chris Mark <chdmark@gmail.com>
2016-03-04 16:10:46 -0800

    Add game variables

--- app/src/main/java/com/christopherdmark/mathsample2/GameActivity.java
+++ app/src/main/java/com/christopherdmark/mathsample2/GameActivity.java
@@ -10,7 +10,16 @@ public class GameActvity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //The next line loads our UI design to the screen
         setContentView(R.layout.activity_game);
+
+        //Here we initialize all our variables
+
+        int partA = 9;
+        int partB = 9;
+        int correctAnswer = partA * partB;
+        int wrongAnswer1 = correctAnswer - 1;
+        int wrongAnswer2 = correctAnswer + 1;
 
     }
 
